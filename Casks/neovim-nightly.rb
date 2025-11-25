54d3f0c21f3cdc54f19fdd55da1282422a945d39
Felix Kästner <felix.kaestner@sap.com>
2025-11-25 23:46:42 +0100

    Initial Commit

---
+++ Casks/neovim-nightly.rb
@@ -0,0 +1,27 @@
+cask "neovim-nightly" do
+  version :latest
+
+  arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+
+  if Hardware::CPU.arm?
+    sha256 "11f87b42529d0c095b26cba4269605b31690d10cafa2dba9b96afbd62c572ae5"
+  else
+    sha256 "7274e1a1325a2cd3d1f4eef27f84d57994b85a55b5b1c3e9d4a37db966b2ba4a"
+  end
+
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz"
+
+  name "Neovim"
+  desc "Ambitious Vim-fork focused on extensibility and agility"
+  homepage "https://neovim.io/"
+
+  binary "nvim-macos-#{arch}/bin/nvim"
+  manpage "nvim-macos-#{arch}/share/man/man1/nvim.1"
+  artifact "nvim-macos-#{arch}/lib/nvim", target: "#{HOMEBREW_PREFIX}/lib/nvim"
+  artifact "nvim-macos-#{arch}/share/nvim", target: "#{HOMEBREW_PREFIX}/share/nvim"
+  artifact "nvim-macos-#{arch}/share/icons/hicolor/128x128/apps/nvim.png", target: "#{HOMEBREW_PREFIX}/share/icons/hicolor/128x128/apps/nvim.png"
+
+  postflight do
+    system_command "xattr", args: ["-c", staged_path.join("nvim-macos-#{arch}", "bin", "nvim")]
+  end
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# homebrew-neovim
+
+homebrew-neovim: A Homebrew tap for installing the latest nightly builds of Neovim.
+
+[![Issues](https://img.shields.io/github/issues/felix-kaestner/homebrew-neovim?color=29b6f6&style=flat-square)](https://github.com/felix-kaestner/yubilock/issues)
+[![License](https://img.shields.io/github/license/felix-kaestner/homebrew-neovim?color=29b6f6&style=flat-square)](https://github.com/felix-kaestner/yubilock/blob/main/LICENSE)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](https://github.com/felix-kaestner/homebrew-neovim/pulls)
+
+## Installation
+
+Install the latest **nightly** version of `neovim` using [Homebrew](https://brew.sh/):
+
+```bash
+# First, tap the repository
+brew tap felix-kaestner/homebrew-neovim https://github.com/felix-kaestner/homebrew-neovim
+
+# Then, install neovim
+brew install --cask felix-kaestner/homebrew-neovim/neovim-nightly
+```
+
+---
+
+Released under the [Apache 2.0](LICENSE).
